1333cda3ce585631489a19ef365d875d7c876e13
oggez <af6260@WCCZC51928CP.ad.mah.se>
2016-03-11 11:08:31 +0100

    Bort med allt test

--- laboration1/QueueException.java
+++
@@ -1,8 +0,0 @@
-package laboration1;
-
-public class QueueException extends RuntimeException {
-    public QueueException() {}
-    public QueueException( String message ) {
-        super( message );
-    }
-}
